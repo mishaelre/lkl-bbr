a4f10c2e2e9d121a64c7ea7b78e47c301462f792
mishaelre <664513537@qq.com>
2017-06-06 23:31:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -118,7 +118,7 @@ server server1 10.0.0.1 maxconn 20480
 
 EOF
 	
-wget --no-check-certificate lkl-bbr
+wget --no-check-certificate https://drive.google.com/open?id=0B9nEQVxNYKsqRGlMZEtkMVNFODQ
 
 cat > /root/lkl/lkl.sh<<-EOF
 LD_PRELOAD=/root/lkl/liblkl-hijack.so LKL_HIJACK_NET_QDISC="root|fq" LKL_HIJACK_SYSCTL="net.ipv4.tcp_congestion_control=bbr;net.ipv4.tcp_wmem=4096 16384 30000000" LKL_HIJACK_OFFLOAD="0x9983" LKL_HIJACK_NET_IFTYPE=tap LKL_HIJACK_NET_IFPARAMS=lkl-tap LKL_HIJACK_NET_IP=10.0.0.2 LKL_HIJACK_NET_NETMASK_LEN=24 LKL_HIJACK_NET_GATEWAY=10.0.0.1 haproxy -f /root/lkl/haproxy.cfg
