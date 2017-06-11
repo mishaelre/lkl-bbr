86e55d81d72a7925a38555b5e4aa03e19a347ed7
mishaelre <664513537@qq.com>
2017-06-11 23:32:30 +0800

    Create install.sh

--- install.sh
+++ install.sh
@@ -201,6 +201,97 @@ EOF
 
 chmod +x /etc/rc.local;echo "sh /root/kcptun/start.sh" >> /etc/rc.local
 
+cd /root
+apt-get install git
+git clone -b manyuser https://github.com/shadowsocksr/shadowsocksr.git
+bash /root/shadowsocksr/initcfg.sh
+
+
+cat > /etc/init.d/shadowsocks<<-EOF
+#!/bin/sh
+# chkconfig: 2345 90 10
+# description: Start or stop the Shadowsocks R server
+#
+### BEGIN INIT INFO
+# Provides: Shadowsocks-R
+# Required-Start: $network $syslog
+# Required-Stop: $network
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Description: Start or stop the Shadowsocks R server
+### END INIT INFO
+
+
+name=shadowsocks
+PY=/usr/bin/python
+SS=/root/shadowsocksr/shadowsocks/server.py
+SSPY=server.py
+conf=/root/shadowsocksr/user-config.json
+
+start(){
+    $PY $SS -c $conf -d start
+    RETVAL=$?
+    if [ "$RETVAL" = "0" ]; then
+        echo "$name start success"
+    else
+        echo "$name start failed"
+    fi
+}
+
+stop(){
+    pid=`ps -ef | grep -v grep | grep -v ps | grep -i "${SSPY}" | awk '{print $2}'`
+    if [ ! -z "$pid" ]; then
+        $PY $SS -c $conf -d stop
+        RETVAL=$?
+        if [ "$RETVAL" = "0" ]; then
+            echo "$name stop success"
+        else
+            echo "$name stop failed"
+        fi
+    else
+        echo "$name is not running"
+        RETVAL=1
+    fi
+}
+
+status(){
+    pid=`ps -ef | grep -v grep | grep -v ps | grep -i "${SSPY}" | awk '{print $2}'`
+    if [ -z "$pid" ]; then
+        echo "$name is not running"
+        RETVAL=1
+    else
+        echo "$name is running with PID $pid"
+        RETVAL=0
+    fi
+}
+
+case "$1" in
+'start')
+    start
+    ;;
+'stop')
+    stop
+    ;;
+'status')
+    status
+    ;;
+'restart')
+    stop
+    start
+    RETVAL=$?
+    ;;
+*)
+    echo "Usage: $0 { start | stop | restart | status }"
+    RETVAL=1
+    ;;
+esac
+exit $RETVAL
+
+EOF
+
+chmod 755 /etc/init.d/shadowsocks ; update-rc.d shadowsocks defaults ; service shadowsocks start
+
+
 #判断是否启动
 p=`ping 10.0.0.2 -c 3 | grep ttl`
 if [ "$p" == "" ]; then
